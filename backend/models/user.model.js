a5f9511446b380d346b44a78fe5282983becb0e8
sagarsuthar2362 <1studywithsagar2362@gmail.com>
2026-04-06 18:11:45 +0530

    feat: created user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter an email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    education: [
+      {
+        institutionName: {
+          type: String,
+          required: true,
+        },
+        degree: {
+          type: String,
+          required: true,
+        },
+        fieldOfStudy: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    expreience: [
+      {
+        companyName: {
+          type: String,
+          required: true,
+        },
+        position: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    profileImage: {
+      type: String,
+      default: "",
+    },
+    coverImage: {
+      type: String,
+      default: "",
+    },
+    connections: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;

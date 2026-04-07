d1711d2331f54a402a621b75f8bff86939d597c7
sagarsuthar2362 <1studywithsagar2362@gmail.com>
2026-04-07 17:58:04 +0530

    feat: implemented secure routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import { userDataContext } from "./context/UserContext";
 
 const App = () => {
+  const { userData } = useContext(userDataContext);
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
+      <Route path="/" element={userData ? <Home /> : <Login />} />
+      <Route path="/login" element={userData ? <Home /> : <Login />} />
+      <Route path="/signup" element={userData ? <Home /> : <Signup />} />
     </Routes>
   );
 };

7df96cb90de2e55eda42054fad05086604d32402
sagarsuthar2362 <1studywithsagar2362@gmail.com>
2026-04-08 20:16:06 +0530

    feat: updated the user routes for the image upload

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,21 @@
 import express from "express";
-import { getCurrentUser } from "../controllers/user.controller.js";
+import {
+  getCurrentUser,
+  updateProfile,
+} from "../controllers/user.controller.js";
+import upload from "../middleware/multer.js";
 import isAuth from "../middleware/isAuth.js";
 const userRouter = express.Router();
 
 userRouter.get("/currentuser", isAuth, getCurrentUser);
+userRouter.post(
+  "/update-profile",
+  isAuth,
+  upload.fields([
+    { name: "profileImage", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  updateProfile,
+);
 
 export default userRouter;

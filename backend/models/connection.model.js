a0406cd1a72e1cbae4440b37bcf3a69f700a2ed5
sagarsuthar2362 <1studywithsagar2362@gmail.com>
2026-04-10 19:04:43 +0530

    feat: created connection schema

---
+++ backend/models/connection.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const connectionSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+    },
+  },
+  { timestamps: true },
+);
+
+const Connection = mongoose.model("Connection", connectionSchema);
+export default Connection;
